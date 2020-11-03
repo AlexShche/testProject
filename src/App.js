e29ec62c95c700e980fdb06b561afbc52ac8a0c3
h_lex <alex.sherbinin99@gmail.com>
2020-11-03 13:09:24 +0500

    Finaly commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import {Prods} from "./Containers/Prods";
+import 'antd/dist/antd.css';
+import './Css/Main.css'
+
+function App() {
+  return (
+    <div className="App">
+        <Prods />
+    </div>
+  );
+}
+
+export default App;
